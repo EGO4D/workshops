e4fcae17fbf6edfdca6c846c2c7a0f53a6cc3875
devanshk <devanshkukreja1@gmail.com>
2022-12-14 12:45:11 -0500

    add eccv22 winners

--- scripts/parse_cvpr22_winners.py
+++ scripts/parse_cvpr22_winners.py
@@ -2,191 +2,331 @@ import pyperclip
 
 
 raw = """
-Challenge: Natural Language Queries 
+Challenge: Ego4D Audio-Visual Diarization Challenge
 
-1st Place: ReLER@ZJU-Alibaba
+1st Place: Intel Labs 
 Team: 
+Kyle Min, Intel Labs
+Validation Report: https://arxiv.org/abs/2210.07764
+Code: https://github.com/SRA2/SPELL
 
-Naiyuan Liu (Zhejiang University, University of Technology Sydney)
-Xiaohan Wang (Zhejiang University)
-Xiaobo Li (Alibaba Group)
-Yi Yang (Zhejiang University)
-Yueting Zhuang (Zhejiang University)
+Challenge: Audio-Only Diarization Challenge
 
-Validation Report: https://arxiv.org/abs/2207.00383
-Code: https://github.com/NNNNAI/Ego4d_NLQ_2022_1st_Place_Solution
-
-2nd Place: EgoVLP
+1st Place: pyannote
 Team: 
+Hervé Bredin, IRIT, Université de Toulouse, CNRS
+Validation report: https://huggingface.co/pyannote/speaker-diarization
+Code: https://huggingface.co/pyannote/speaker-diarization
 
-Kevin Qinghong Lin (NUS)
-Alex Jinpeng Wang (NUS)
-Mattia Soldan (KAUST)
-Michael Wray (University of Bristol)
-Rui Yan (NUS)
-Eric Zhongcong Xu (NUS)
-Difei Gao (NUS)
-Rongcheng Tu (Tencent)
-Wenzhe Zhao (Tencent)
-Weijie Kong (Tencent)
-Chengfei Cai (Tencent)
-Hongfa Wang (Tencent)
-Dima Damen (University of Bristol)
-Bernard Ghanem (KAUST)
-Wei Liu (Tencent)
-Mike Zheng Shou (NUS)
-
-Validation Report: https://arxiv.org/abs/2207.01622
-Code: https://github.com/showlab/EgoVLP
-
-3rd Place: MSRA-AIM3_Teams
+2nd Place: diart
 Team: 
-Sipeng Zheng, Renmin University of China
-Qi Zhang, Renmin University of China
-Bei Liu, Microsoft Research
-Qin Jin, Renmin University of China
-Jianlong Fu, Microsoft Research
+Juan M. Coria, Universite Paris-Saclay CNRS, LISN
+Sahar Ghannay, Universite Paris-Saclay CNRS, LISN
+Validation report: https://github.com/juanmc2005/StreamingSpeakerDiarization/blob/ego4d/ego4d.pdf
+Code: https://github.com/juanmc2005/StreamingSpeakerDiarization/tree/ego4d
 
-Validation Report: Forthcoming 
-Code: Forthcoming 
-
-Challenge: PNR Temporal Localization
-
-1st Place: SViT
+3rd Place: Red Panda@IMAGINE
 Team: 
+Yin-Dong Zheng, Nanjing University
+Guo Chen, Nanjing University, Shanghai AI Laboratory
+Jiahao Wang, Nanjing University
+Tong Lu, Nanjing University
+Limin Wang, Nanjing University, Shanghai AI Laboratory
+Validation report: https://arxiv.org/abs/2211.08708
+Code: https://github.com/Echo0125/Ego4d_Audio-only_Diarization_2022_3rd_Place_Solution
+ 
 
-Elad Ben-Avraham, Tel Aviv University
-Roei Herzig, Tel Aviv University
-Karttikeya Mangalam, UC Berkeley
-Amir Bar, Tel Aviv University
-Anna Rohrbach, UC Berkeley
-Leonid Karlinsky, IBM-Research
-Trevor Darrell, UC Berkeley
-Amir Globerson, Tel Aviv University, Google
+Challenge: AV Transcription Challenge
 
-
-
-Validation Report: https://arxiv.org/pdf/2206.07689.pdf
-Code: https://eladb3.github.io/SViT/
-
-2nd Place: UniAndes
+1st Place:  AVATAR-Google
 Team: 
+Paul Hongsuck Seo, Google Research
+Arsha Nagrani, Google Research
+Cordelia Schmid, Google Research
+Validation report: https://arxiv.org/pdf/2211.09966.pdf
 
-Maria Escobar, Universidad de los Andes
-Laura Daza, Universidad de los Andes
-Cristina González, Universidad de los Andes
-Jordi Pont-Tuset, Google
-Pablo Arbelaez, Universidad de los Andes
+Challenge: Visual Queries 2D Localization
 
-Validation Report: https://arxiv.org/pdf/2207.11329.pdf 
-Code: https://github.com/BCV-Uniandes/PNR_OSCC 
-
-3rd Place: EgoVLP
-Team: 
-
-Kevin Qinghong Lin (NUS)
-Alex Jinpeng Wang (NUS)
-Mattia Soldan (KAUST)
-Michael Wray (University of Bristol)
-Rui Yan (NUS)
-Eric Zhongcong Xu (NUS)
-Difei Gao (NUS)
-Rongcheng Tu (Tencent)
-Wenzhe Zhao (Tencent)
-Weijie Kong (Tencent)
-Chengfei Cai (Tencent)
-Hongfa Wang (Tencent)
-Dima Damen (University of Bristol)
-Bernard Ghanem (KAUST)
-Wei Liu (Tencent)
-Mike Zheng Shou (NUS)
-
-Validation Report: https://arxiv.org/abs/2207.01622
-Code: https://github.com/showlab/EgoVLP
-
-Challenge: Object State Change Classification
-
-1st Place: EgoVLP
-Team: 
-Kevin Qinghong Lin (NUS)
-Alex Jinpeng Wang (NUS)
-Mattia Soldan (KAUST)
-Michael Wray (University of Bristol)
-Rui Yan (NUS)
-Eric Zhongcong Xu (NUS)
-Difei Gao (NUS)
-Rongcheng Tu (Tencent)
-Wenzhe Zhao (Tencent)
-Weijie Kong (Tencent)
-Chengfei Cai (Tencent)
-Hongfa Wang (Tencent)
-Dima Damen (University of Bristol)
-Bernard Ghanem (KAUST)
-Wei Liu (Tencent)
-Mike Zheng Shou (NUS)
-
-Validation Report: https://arxiv.org/abs/2207.01622
-Code: https://github.com/showlab/EgoVLP
-
-2nd Place: TarHeels
-Team: 
-Md Mohaiminul Islam, UNC Chapel Hill
-Gedas Bertasius, UNC Chapel Hill
-
-Validation Report: https://arxiv.org/abs/2207.11814 
-Code: https://github.com/md-mohaiminul/ObjectStateChange 
-
-3rd Place: UniAndes
-Team: 
-Laura Daza, Universidad de los Andes
-Maria Escobar, Universidad de los Andes
-Cristina González, Universidad de los Andes
-Jordi Pont-Tuset, Google
-Pablo Arbelaez, Universidad de los Andes
-Validation Report: https://arxiv.org/pdf/2207.11329.pdf 
-Code: https://github.com/BCV-Uniandes/PNR_OSCC 
-
-Challenge: Long Term Anticipation 
-
-1st Place: Autonomous Systems
-Team: 
-Esteve Valls Mascaro, Technische Universtität Wien
-Hyemin Ahn, Ulsan National Institute of Science and Technology (UNIST)
-Dongheui Lee, Technische Universtität Wien
-Validation Report: https://arxiv.org/abs/2207.12080
-Code: https://github.com/Evm7/ego4dlta-icvae
-
-
-2nd Place: RoboVision
-Team: 
-Srijan Das, Stony Brook University
-Michael Ryoo, Stony Brook University, Google
-Validation Report: https://arxiv.org/abs/2207.00579
-Code: https://github.com/srijandas07/clip_baseline_LTA_Ego4d
-
-Challenge: Visual Queries
-
-1st Place: thereisnospoon
+1st Place: Thereisnospoon
 Team: 
 Mengmeng Xu, KAUST
 Juan-Manuel Perez-Rua, Facebook
 Cheng-Yang Fu, Facebook
 Yanghao Li, Facebook
-Bernard Ghanem, KAUST
+ Bernard Ghanem, KAUST
 Tao Xiang, Facebook
-
-Validation Report: Forthcoming 
+Validation report: https://arxiv.org/abs/2211.10528 
 Code: https://github.com/facebookresearch/vq2d_cvpr
+
+
+Challenge: Visual Queries 3D Localization
+
+1st Place: IVUL
+Team: 
+Jinjie Mai, KAUST
+Chen Zhao, KAUST
+Abdullah Hamdi, KAUST
+Silvio Giancola, KAUST
+Bernard Ghanem, KAUST 
+Validation report: https://arxiv.org/pdf/2211.10284.pdf
+Code: https://github.com/Wayne-Mai/VQ3D_ECCVW2022
+
+2nd Place: Thereisnospoon
+Team: 
+Mengmeng Xu, KAUST
+Juan-Manuel Perez-Rua, Facebook
+Cheng-Yang Fu, Facebook
+Yanghao Li, Facebook
+ Bernard Ghanem, KAUST
+Tao Xiang, Facebook
+Validation report: https://arxiv.org/abs/2211.10528
+Code: https://github.com/facebookresearch/vq2d_cvpr
+
+Challenge: Natural Language Queries
+
+1st Place: VideoIntern
+Team: 
+Guo Chen, Shanghai AI Laboratory, Nanjing University
+Jiahao Wang, Nanjing University
+Yi Liu, SIAT, Shanghai AI Laboratory
+Yifei Huang, University of Tokyo
+Jiashuo Yu, Shanghai AI Laboratory
+Yi Wang, Shanghai AI Laboratory
+Yali Wang, SIAT, Shanghai AI Laboratory
+Tong Lu, Nanjing University
+Limin Wang, Nanjing University, Shanghai AI Laboratory
+Yu Qiao, Shanghai AI Laboratory
+Validation report: https://arxiv.org/abs/2211.09529
+Code: https://github.com/OpenGVLab/ego4d-eccv2022-solutions
+
+2nd Place: University of Wisconsin-Madison
+Team: 
+Sicheng Mo, University of Wisconsin-Madison
+Fangzhou Mu, University of Wisconsin-Madison
+Yin Li, University of Wisconsin-Madison
+Validation report: https://arxiv.org/pdf/2211.08704.pdf
+
+
+3rd Place: COarse-to-fiNE alignment framework (CONE)
+Team: 
+Zhijian Hou, City University of Hong Kong
+Wanjun Zhong, Sun Yat-sen University
+Lei Ji, Microsoft Research Asia
+Difei Gao, National University of Singapore
+Kun Yan, Beihang University
+Wing-Kwong Chan, City University of Hong Kong
+Chong-Wah Ngo, Singapore Management University
+Zheng Shou, National University of Singapore
+Nan Duan,  Microsoft Research Asia
+Validation report: https://arxiv.org/pdf/2211.08776.pdf
+Code: https://github.com/houzhijian/CONE.
+
+Challenge: Moments Queries
+
+1st Place: VideoIntern
+Team: 
+Guo Chen, Shanghai AI Laboratory, Nanjing University
+Jiahao Wang, Nanjing University
+Yi Liu, SIAT, Shanghai AI Laboratory
+Yifei Huang, University of Tokyo
+Jiashuo Yu, Shanghai AI Laboratory
+Yi Wang, Shanghai AI Laboratory
+Yali Wang, SIAT, Shanghai AI Laboratory
+Tong Lu, Nanjing University
+Limin Wang, Nanjing University, Shanghai AI Laboratory
+Yu Qiao, Shanghai AI Laboratory
+Validation report: https://arxiv.org/abs/2211.09529
+Code: https://github.com/OpenGVLab/ego4d-eccv2022-solutions
+2nd Place: University of Wisconsin-Madison
+Team: 
+Fangzhou Mu, University of Wisconsin-Madison
+ Sicheng Mo, University of Wisconsin-Madison
+Gillian Wang, University of Wisconsin-Madison
+Yin Li, University of Wisconsin-Madison
+Validation report: https://arxiv.org/pdf/2211.09074.pdf
+Code: https://github.com/happyharrycn/actionformer_release
+
+3rd Place: ReLER@ZJU
+Team: 
+Jiayi Shao, ReLER Lab, CCAI, Zhejiang University
+Xiaohan Wang, ReLER Lab, CCAI, Zhejiang University
+Yi Yang, ReLER Lab, CCAI, Zhejiang University
+Validation report: https://arxiv.org/abs/2211.09558
+Code: https://github.com/JonnyS1226/Ego4d_mq_3rd_solution
+
+Challenge: Looking at Me 
+
+1st Place: PKU-WICT-MIPL
+Team: 
+Xiyu Wei, Wangxuan Institute of Computer Technology, Peking University
+Dejie Yang, Wangxuan Institute of Computer Technology, Peking University
+Minghang Zheng, Wangxuan Institute of Computer Technology, Peking University
+Qingchao Chen, National Institute of Health Data Science, Peking University
+Yuxin Peng, Wangxuan Institute of Computer Technology, Peking University 
+Yang Liu, Wangxuan Institute of Computer Technology, Peking University; Beijing Institute for General Artificial Intelligence
+Code: https://github.com/lemon-prog123/Ego4D-ECCV-LAM
+
+2nd Place: KeioEgo
+Team: 
+Haowen Hu, Graduate School of Science and Technology, Keio University
+Ryo Hachiuma, Graduate School of Science and Technology, Keio University 
+Hideo Saito, Graduate School of Science and Technology, Keio University
+Validation report: https://drive.google.com/file/d/1N8OsZ77gUTcGJ1s8TEjKk8T_o54Fifv5/view?usp=sharing
+Research code: https://github.com/Huhaowen0130/EgoFlow
+
+3rd Place: NPT
+Team: 
+Yinan He, Beijing University of Posts and Telecommunications
+Guo Chen, State Key Lab for Novel Software Technology, Nanjing University
+Validation report: https://github.com/yinanhe/ego4d-look-at-me/blob/master/NPT_LAM.pdf
+Research code: https://github.com/yinanhe/ego4d-look-at-me
+
+Challenge: Talking to Me 
+
+1st Place: University of Texas at Austin & Meta AI 
+Team: 
+Zihui Xue, The University of Texas at Austin and Meta AI 
+Yale Song, Meta AI 
+Kristen Grauman, The University of Texas at Austin and Meta AI 
+Validation report: Forthcoming 
+
+
+Challenge: Long Term Action Anticipation
+
+1st Place: Autonomous Systems 
+Team: 
+Esteve Valls Mascaro, Autonomous Systems, Technische Universitat Wien (TU Wien)
+Hyemin Ahn, Ulsan National Institute of Science and Technology (UNIST)
+Dongheui Lee, Autonomous Systems, Technische Universitat Wien (TU Wien) & Institute of Robotics and Mechatronics, German Aerospace Center (DLR)
+Validation report: https://arxiv.org/abs/2207.12080
+
+Challenge: Future Hand Prediction
+
+1st Place: VideoIntern 
+Team: 
+Guo Chen, Shanghai AI Laboratory, Nanjing University
+Yizhuo Li, The University of Hong Kong, Shanghai AI Laboratory
+Kunchang Li, SIAT, Shanghai AI Laboratory
+Yinan He, Shanghai AI Laboratory
+Bingkun Huang, Nanjing University, Shanghai AI Laboratory
+Yifei Huang, University of Tokyo
+Yi Wang, Shanghai AI Laboratory
+Yali Wang, SIAT, Shanghai AI Laboratory
+Tong Lu, Nanjing University
+Limin Wang, Nanjing University, Shanghai AI Laboratory
+Yu Qiao, Shanghai AI Laboratory
+Validation report: https://arxiv.org/abs/2211.09529
+Code: https://github.com/OpenGVLab/ego4d-eccv2022-solutions
+
+2nd Place: KeioEgo
+Team: 
+Haowen Hu, Graduate School of Science and Technology, Keio University
+Ryo Hachiuma, Graduate School of Science and Technology, Keio University 
+Hideo Saito, Graduate School of Science and Technology, Keio University
+Code: https://github.com/masashi-hatano/ego4d-fhp-challenge-2022
+
+Challenge:  Short Term Object Interaction Anticipation 
+
+1st Place: Video Intern 
+Team: 
+Sen Xing, Shanghai AI Laboratory, Tsinghua University
+Guo Chen, Nanjing University, Shanghai AI Laboratory
+Zhe Chen, Nanjing University, Shanghai AI Laboratory
+Junting Pan, Chinese University of Hong Kong, Shanghai AI Laboratory
+Yifei Huang, University of Tokyo
+Yi Wang, Shanghai AI Laboratory
+Yali Wang, SIAT, Shanghai AI Laboratory
+Limin Wang, Nanjing University, Shanghai AI Laboratory
+Yu Qiao, Shanghai AI Laboratory
+Validation report: https://arxiv.org/abs/2211.09529
+Code: https://github.com/OpenGVLab/ego4d-eccv2022-solutions
+
+Challenge: State Change Object Detection
+
+1st Place: VideoIntern
+Team: 
+Guo Chen, Shanghai AI Laboratory, Nanjing University
+Zhe Chen, Nanjing University, Shanghai AI Laboratory
+Yi Wang, Shanghai AI Laboratory
+Wenhai Wang, Shanghai AI Laboratory
+Yali Wang, SIAT, Shanghai AI Laboratory
+Limin Wang, Nanjing University, Shanghai AI Laboratory
+Yu Qiao, Shanghai AI Laboratory
+Validation report: https://arxiv.org/abs/2211.09529
+Code: https://github.com/OpenGVLab/ego4d-eccv2022-solutions
+
+Challenge: Object State Change Classification
+
+1st Place: Red Panda@IMAGINE 
+Team: 
+Yin-Dong Zheng, Nanjing University
+Guo Chen, Nanjing University, Shanghai AI Laboratory
+Jiahao Wang, Nanjing University
+Tong Lu, Nanjing University
+Limin Wang, Nanjing University, Shanghai AI Laboratory
+Validation report: https://arxiv.org/pdf/2211.08728.pdf
+
+2nd Place: EgoMotion-COMPASS 
+Team: 
+Jianchen Lei, Zhejiang University 
+Shuang Ma, Microsoft
+Zhongjie Ba, Zhejiang University 
+Kui Ren, Zhejiang University
+Validation report: https://arxiv.org/abs/2211.15286
+Code: https://github.com/jasonrayshd/egomotion
+
+
+3rd Place: Pear Republic 
+Team: 
+Shijie Wang, Brown University  
+Validation report: https://drive.google.com/file/d/1L8mEDXlStom-APCEkwfrNM0Vd5aGIGUp/view?usp=sharing
+
+
+Challenge: PNR Temporal Localization
+
+1st Place: Red Panda@IMAGINE 
+Team: 
+Yin-Dong Zheng, Nanjing University
+Guo Chen, Nanjing University, Shanghai AI Laboratory
+Jiahao Wang, Nanjing University
+Tong Lu, Nanjing University
+Limin Wang, Nanjing University, Shanghai AI Laboratory
+Validation report: https://arxiv.org/pdf/2211.08728.pdf
+
+2nd Place: EgoMotion-COMPASS
+Team: 
+Jianchen Lei, Zhejiang University 
+Shuang Ma, Microsoft
+Zhongjie Ba, Zhejiang University 
+Kui Ren, Zhejiang University 
+Valdition report: https://github.com/jasonrayshd/egomotion 
+
+3rd Place: University of Texas at Austin & Meta AI 
+Team: 
+Zihui Xue, The University of Texas at Austin and Meta AI 
+Yale Song, Meta AI 
+Kristen Grauman, The University of Texas at Austin and Meta AI 
+Validation report: Forthcoming 
 """
 
 output_html = ""
 
 evalai_links = {
+    "Ego4D Audio-Visual Diarization Challenge": "https://eval.ai/web/challenges/challenge-page/1640/overview",
+    "Audio-Only Diarization Challenge": "https://eval.ai/web/challenges/challenge-page/1641/overview",
+    "AV Transcription Challenge": "https://eval.ai/web/challenges/challenge-page/1637/overview",
+    "Visual Queries 2D Localization": "https://eval.ai/web/challenges/challenge-page/1843/overview",
+    "Visual Queries 3D Localization": "https://eval.ai/web/challenges/challenge-page/1646/overview",
+    "Moments Queries": "https://eval.ai/web/challenges/challenge-page/1626/overview",
+    "Looking at Me": "https://eval.ai/web/challenges/challenge-page/1624/overview",
+    "Talking to Me": "https://eval.ai/web/challenges/challenge-page/1625/overview",
+    "Long Term Action Anticipation": "https://eval.ai/web/challenges/challenge-page/1598/overview",
+    "Future Hand Prediction": "https://eval.ai/web/challenges/challenge-page/1630/overview",
+    "Short Term Object Interaction Anticipation": "https://eval.ai/web/challenges/challenge-page/1623/overview",
+    "State Change Object Detection": "https://eval.ai/web/challenges/challenge-page/1632/overview",
     "Natural Language Queries": "https://eval.ai/web/challenges/challenge-page/1629/overview",
-    "PNR Temporal Localization": "https://eval.ai/web/challenges/challenge-page/1622/overview",
     "Object State Change Classification": "https://eval.ai/web/challenges/challenge-page/1627/overview",
-    "Long Term Anticipation": "https://eval.ai/web/challenges/challenge-page/1598/overview",
-    "Visual Queries": "https://eval.ai/web/challenges/challenge-page/1619/overview",
+    "PNR Temporal Localization": "https://eval.ai/web/challenges/challenge-page/1622/overview",
 }
 
 newline = "\n"
@@ -241,33 +381,33 @@ for line in raw.split("\n"):
     </tr>"""
         last_context = "challenge"
 
-    elif line.startswith("Team"):
+    elif line.lower().startswith("team"):
         context["team_members"] = []
         last_context = "team_members"
 
-    elif line.startswith("Validation Report"):
+    elif line.lower().startswith("validation report"):
         context["validation_report"] = line[line.index(":") + 1 :]
         last_context = "validation_report"
 
-    elif line.startswith("Code"):
+    elif line.lower().startswith("code"):
         context["code"] = line[line.index(":") + 1 :]
         last_context = "code"
 
-    elif line.startswith("1st Place"):
+    elif line.lower().startswith("1st place"):
         if last_context != "challenge":
             output_html += gen_html(context)
         context["position"] = "1st Place"
         context["team_name"] = line.split(":")[1].strip()
         last_context = "position"
 
-    elif line.startswith("2nd Place"):
+    elif line.lower().startswith("2nd place"):
         if last_context != "challenge":
             output_html += gen_html(context)
         context["position"] = "2nd Place"
         context["team_name"] = line.split(":")[1].strip()
         last_context = "position"
 
-    elif line.startswith("3rd Place"):
+    elif line.lower().startswith("3rd place"):
         if last_context != "challenge":
             output_html += gen_html(context)
         context["position"] = "3rd Place"
